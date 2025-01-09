e7368aa3cb86f9e4276f7cf7258247c2405afe43
Starsaddict <18201122059zky@gmail.com>
2025-01-09 13:16:52 +0100

    Panier

---
+++ src/Requets/Panier.java
@@ -0,0 +1,12 @@
+package Requets;
+
+public class Panier {
+
+    static String url = "jdbc:mysql://sql7.freesqldatabase.com:3306/sql7756463";
+    static String user = "sql7756463";
+    static String mdp = "iFgwWVZFHW";
+
+    public static void main(String[] args) {
+
+    }
+}
