2210ab64e67eb992825609d525b9fc66d5357fd9
Starsaddict <18201122059zky@gmail.com>
2025-01-09 14:55:45 +0100

    US 0.4

--- src/Requets/Produits.java
+++ src/Requets/Produits.java
@@ -1,14 +1,15 @@
 package Requets;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import connexion.Connect;
+import java.sql.*;
+import javax.swing.*;
 
 public class Produits {
+    static String url = "jdbc:mysql://sql7.freesqldatabase.com:3306/sql7756463";
+    static String user = "sql7756463";
+    static String mdp = "iFgwWVZFHW";
 
     //US 0.1 Je veux visualiser les détails d'un produit : prix unitaire, prix au kg, nutriscore, libellé article, poids, condionnement, ...
-    public static void visualiser ( int idProd) throws SQLException{
+    public static void visualiser ( int idProd){
         String libelle = "";
         String main_category = "";
         String sub_category = "";
@@ -22,9 +23,11 @@ public class Produits {
 
         String query_all = "SELECT * FROM produit JOIN categories ON produit.category = categories.Id_cat";
 
+        try (Connection con = DriverManager.getConnection(url, user, mdp);
+             Statement stm = con.createStatement())
+        {
 
-            ResultSet res_all = Connect.executeQuery(query_all + " " + where);
-            
+            ResultSet res_all = stm.executeQuery(query_all + " " + where);
             if (res_all.next()) {
                 libelle = res_all.getString("name");
                 main_category = res_all.getString("main_category");
@@ -33,9 +36,11 @@ public class Produits {
                 nb_reviews = res_all.getInt("no_of_ratings");
                 discounted_price = res_all.getInt("discount_price");
                 actual_price = res_all.getInt("actual_price");
-
             }
-       
+        } catch (SQLException e) {
+            System.out.println("Requete/Syntaxe incorrect");
+            e.printStackTrace();
+        }
         if (actual_price != 0) {
             discount_percentage = discounted_price * 100 / actual_price;
         } else {
@@ -55,12 +60,14 @@ public class Produits {
     }
 
     //US 0.4 Je veux trier une liste de produits
-    public static void trierProduits(int idCat) throws SQLException{
+    public static void trierProduits(int idCat, String trier_par, String trier_ord){
         String where = "WHERE category = " + idCat;
         String query = "SELECT name FROM produit JOIN categories ON produit.category = categories.Id_cat";
-
-        
-            ResultSet res_all = Connect.executeQuery(query + " " + where);
+        String trier = "ORDER BY " + trier_par + " " + trier_ord;
+        try (Connection con = DriverManager.getConnection(url, user, mdp);
+            Statement stm = con.createStatement())
+        {
+            ResultSet res_all = stm.executeQuery(query + " " + where);
             int count = 1;
             while (res_all.next()) {
                 System.out.print(count + ". ");
@@ -68,12 +75,37 @@ public class Produits {
                 count++;
             }
 
+        }catch (SQLException e) {
+            System.out.println("Requete/Syntaxe incorrect");
+            e.printStackTrace();
+        }
     }
-    public static void main(String[] args) throws SQLException {
+    
+    public static void trier(int idCat){
+
+        JPanel panel = new JPanel();
+        String[] options = {"libelle", "rating", "price"};
+        JComboBox<String> comboBox = new JComboBox<>(options);
+        panel.add(comboBox);
+
+        String[] ordre = {"ASC", "DESC"};
+        JComboBox<String> comboBox2 = new JComboBox<>(ordre);
+        panel.add(comboBox2);
+
+        int result = JOptionPane.showConfirmDialog(null, panel, "Comment tirer ?", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            String trier_par = comboBox.getSelectedItem().toString();
+            String trier_ord = comboBox2.getSelectedItem().toString();
+            trierProduits(idCat, trier_par, trier_ord);
+        }
+    }
+    public static void main(String[] args) {
         // exemple de utilisation de US 0.1
 //        visualiser(3);
 
         // exemple de utilisation de US 0.4
-        trierProduits(1);
+//        trierProduits(1, "name", "ASC");
+
+        trier(1);
     }
 }
