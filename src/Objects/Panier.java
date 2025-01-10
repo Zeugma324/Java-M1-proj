34d2f7a41de01736ec72cc1123d688fce33fc628
Starsaddict <18201122059zky@gmail.com>
2025-01-10 17:17:49 +0100

    User_change

--- src/Objects/Panier.java
+++ src/Objects/Panier.java
@@ -15,12 +15,14 @@ public class Panier {
 
     private List<Produit> produit = new ArrayList<>();
     private int id;
-    private String time;
+    private String start_time;
     private User user;
+    private String end_time;
+    private boolean isactive = true;
 
     public Panier(int id, User user) {
         this.id = id;
-        this.time = new String(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        this.start_time = new String(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
         this.user = user;
     }
 
@@ -33,18 +35,28 @@ public class Panier {
     }
 
     public void addProduit(Produit prd) throws SQLException {
-        this.produit.add(prd);
-        String query = "INSERT INTO panier VALUES (" + this.id + ", " + prd.getId() + ", " + prd.getQteStocke() + ", '" + this.time + "' , NULL, " + user.getId() + ")";
-        Connect.executeUpdate(query);
+        Optional<Produit> search_prod = this.produit.stream()
+                .filter(p -> p.getId() == prd.getId())
+                .findFirst();
+        if(search_prod.isPresent()) {
+            search_prod.get().setQteStocke(search_prod.get().getQteStocke()+1);
+            Connect.executeUpdate("UPDATE panier SET qte_produit = qte_produit + 1 WHERE Id_produit = " + prd.getId() + " AND Id_panier = " + this.id + " AND Id_user = " + user.getId() + " AND Date_debut = '" + this.start_time + "' )");
+        }
+        else {
+            this.produit.add(prd);
+            String query = "INSERT INTO panier VALUES (" + this.id + ", " + prd.getId() + ", " + prd.getQteStocke() + ", '" + this.start_time + "' , NULL, " + user.getId() + ")";
+            Connect.executeUpdate(query);
+        }
     }
 
     public void removeProduit(Produit prd) throws SQLException{
         this.produit.remove(prd);
-        String query = "DELETE FROM Panier WHERE Id_produit = " + prd.getId() + " AND Id_panier = " + this.id + " AND Id_user = " + user.getId() + " AND Date_debut = '" + this.time + "' )";
+        String query = "DELETE FROM Panier WHERE Id_produit = " + prd.getId() + " AND Id_panier = " + this.id + " AND Id_user = " + user.getId() + " AND Date_debut = '" + this.start_time + "' )";
         Connect.executeUpdate(query);
     }
 
-    public Produit getReplacment(Produit prd) throws SQLException {
+    public void getReplacment(Produit prd) throws SQLException {
+        removeProduit(prd);
         List<Produit> base_list = new ArrayList<>();
         String str = "SELECT p.*, pa.qte_produit FROM panier pa JOIN produit p ON pa.Id_produit = p.Id_produit WHERE pa.Id_user = " + user.getId() + " AND pa.Date_fin IS NOT NULL";
         ResultSet res = Connect.executeQuery(str);
@@ -63,9 +75,25 @@ public class Panier {
         ResultSet res_2 = Connect.executeQuery(end_query);
         while(res_2.next()) {
             Produit new_prod = new Produit(res_2.getInt("Id_produit"),res_2.getString("name"),res_2.getDouble("ratings"), res_2.getInt("no_of_ratings"), res_2.getInt("discount_price"), res_2.getInt("actual_price"), res_2.getInt("category"), res_2.getInt("qte_produit"));
-            return new_prod;
+            addProduit(new_prod);
+            System.out.println(new_prod.toString());
         }
-        return null;
     }
 
+    public void validate() throws SQLException {
+        this.end_time = new String(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        isactive = false;
+        String str_panier = "UPDATE panier SET Date_fin = " + this.end_time + " WHERE Id_panier = " + this.id;
+        Connect.executeUpdate(str_panier);
+        String str_command_table = "INSERT INTO commande (Date_commande) VALUES (" + this.end_time + " )";
+        String str_association_table = "INSERT INTO PanierCommande (panier_id, Id_commande) VALUES (" + this.id +
+                " , SELECT Id_commande FROM commande WHERE Date_commande = " + this.end_time + " )";
+        Connect.executeUpdate(str_command_table);
+        Connect.executeUpdate(str_association_table);
+    }
+
+    public void delete() throws SQLException{
+        this.produit.clear();
+        Connect.executeUpdate("DELETE FROM panier WHERE Id_panier = " + this.id);
+    }
 }
--- src/Objects/User.java
+++ src/Objects/User.java
@@ -5,17 +5,31 @@ import connexion.Connect;
 import java.sql.*;
 
 public class User {
-    private int idUser; // Correspond à Id_user
-    private String lastname; // Correspond à lastname
-    private String name; // Correspond à name
-    private String tel; // Correspond à tel
-    private String address; // Correspond à adress
+    private int idUser;
+    private String lastname;
+    private String name;
+    private String tel;
+    private String address;
 
-    private User(String name, String lastname) throws SQLException {
-        this.lastname = lastname;
-        this.name = name;
+    private User(int idUser) throws SQLException {
+        this.idUser = idUser;
+        String query = "SELECT * FROM utilisateur WHERE id_user = " + idUser + ";";
+        ResultSet result = Connect.executeQuery(query);
+            if (result.next()) {
+                name = result.getString("name");
+                lastname = result.getString("lastname");
+                address = result.getString("adress");
+                tel = result.getString("tel");
+            }
+    }
 
-        String query = "INSERT INTO utilisateur (name, lastname) VALUES ( "+ name +"," +lastname+ ");";
+    public static User findUtilisateur(int idUser) throws SQLException {
+        return new User(idUser);
+    }
+
+    private static int addUserDB(String name, String lastname) throws SQLException {
+        int idUser = 0;
+        String query = "INSERT INTO utilisateur (name, lastname) VALUES ('" + name + "', '" + lastname + "');";
         try (Connection con = Connect.getConnexion();
              PreparedStatement pstmt = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
             int affectedRows = pstmt.executeUpdate();
@@ -24,26 +38,26 @@ public class User {
             }
             try (ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
                 if (generatedKeys.next()) {
-                    this.idUser = generatedKeys.getInt(1);
+                    idUser = generatedKeys.getInt(1);
                 } else {
                     throw new SQLException("Creating user failed, no ID obtained.");
                 }
             }
-        } catch (SQLException e) {
-            System.err.println("SQL Error during user creation: " + e.getMessage());
-            throw e;
         }
+        return idUser;
     }
 
 
     public static User createUtilisateur(String name, String lastname) {
         try {
-            return new User(name, lastname);
+            int idUser = addUserDB(name,lastname);
+            return new User(idUser);
         } catch (SQLException e) {
             System.out.println("Failed to create user: " + e.getMessage());
             return null;
         }
     }
+
 
 
     public void update( String key, String value){
@@ -94,5 +108,16 @@ public class User {
                 ", name='" + name + '\'' +
                 ", lastname='" + lastname + '\'' +
                 '}';
+    }
+    public static void main(String[] args) throws SQLException {
+//        User user = User.findUtilisateur(1);
+//        System.out.println(user);
+
+        User user1 = User.createUtilisateur("Kaiyang","ZHANG");
+
+        System.out.println(user1);
+        user1.setAdress("30 Ave chocolat");
+        user1.setTel("0612345678");
+        System.out.println(user1);
     }
 }
