bc3f24675b5f1e9f0de15510b94c9d6ba84c231e
Yihan332 <zhangyihan332@gmail.com>
2025-01-13 15:40:19 +0100

    yihan

--- src/Requets/USyihan.java
+++ src/Requets/USyihan.java
@@ -2,183 +2,138 @@ package Requets;
 
 import java.sql.*;
 import java.util.Scanner;
+import connexion.Connect;
+
 
 public class USyihan {
-    static String url = "jdbc:mysql://sql7.freesqldatabase.com:3306/sql7756463";
-    static String user = "sql7756463";
-    static String mdp = "iFgwWVZFHW";
+
     // us0.2 recherche produits par mot-cle
-    public static void rechercherProduitParMotCle(String motCle) {
+    public static void rechercherProduitParMotCle(String motCle) throws SQLException{
+        String sql = "SELECT P.name FROM produit P WHERE P.name LIKE '%" + motCle + "%'";
 
-        // SQL查询
-        String sql02 = "SELECT P.name FROM produit P WHERE P.name LIKE '%' + motCle + '%'";
+        ResultSet result = Connect.executeQuery(sql);
 
-        // 使用 Connect 类来获取连接
-        try (Connection con = DriverManager.getConnection(url, user, mdp);
-             Statement stm = con.createStatement();){
-
-            // 执行查询
-            ResultSet resultSet = stm.executeQuery(sql02);
-
-            // 输出结果
-            System.out.println("Produits trouvés :");
-            while (resultSet.next()) { // 遍历结果集
-                String produit = resultSet.getString("name"); // 获取查询结果中的产品名称
-                System.out.println(" " + produit); //打印每个产品名称
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+        System.out.println("Produits trouvés :");
+        while (result.next()) {
+            String produit = result.getString("name");
+            System.out.println(" " + produit);
         }
+
+        Connect.closeConnexion();
     }
 
+
     // us1.5 reprendre panier en cours
-    public static void PanierEnCours(int userID){
+    public static void PanierEnCours(int userID) throws SQLException{
 
-        String sql15 = "SELECT id_panier, Date_debut FROM panier WHERE Id_user = ? AND Date_fin IS NULL";
+        String sql = "SELECT id_panier, Date_debut FROM panier WHERE Id_user = " + userID + " AND Date_fin IS NULL";
 
-        try (Connection con = DriverManager.getConnection(url, user, mdp);
-             PreparedStatement statement = con.prepareStatement(sql15)) {
+        ResultSet result = Connect.executeQuery(sql);
 
-            statement.setInt(1, userID); //设置查询参数
+        if (result.next()) {
+            int panierID = result.getInt("id_Panier");//获取购物车id
+            String DateDebut = result.getString("Date_debut"); //获取该购物车开始时间
 
-            ResultSet resultSet = statement.executeQuery();
+            // 打印购物车信息
+            System.out.println("Panier en cours trouvé :");
+            System.out.println("ID Panier: " + panierID);
+            System.out.println("Date de début: " + DateDebut);
 
-            if (resultSet.next()) {
-                int panierID = resultSet.getInt("id_Panier");//获取购物车id
-                String DateDebut = resultSet.getString("Date_debut"); //获取该购物车开始时间
-
-                // 打印购物车信息
-                System.out.println("Panier en cours trouvé :");
-                System.out.println("ID Panier: " + panierID);
-                System.out.println("Date de début: " + DateDebut);
-
-            }else{
-                System.out.println("Aucun panier en cours trouvé pour User: " + userID);
-            }
-
-        }catch (SQLException e) {
-            e.printStackTrace();
+        }else{
+            System.out.println("Aucun panier en cours trouvé pour User: " + userID);
         }
+
+        Connect.closeConnexion();
+
+
     }
 
     //us3.3 修改产品库存、类别、客户的数据 editer les statistique sur produits，category...
     //显示产品原始库存数量 affichier les QteStock Original
-    public static void QteStock(int produitID){
+    public static void QteStock(int produitID) throws SQLException{
 
-        String sql = "SELECT Id_produit, quantity FROM stock WHERE Id_produit = ?";
+        String sql = "SELECT Id_produit, quantity FROM stock WHERE Id_produit = "  + produitID;
 
-        try (Connection connection = DriverManager.getConnection(url, user, mdp);
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, produitID);
+        ResultSet result = Connect.executeQuery(sql);
 
-            ResultSet resultSet = statement.executeQuery();
-
-            if(resultSet.next()){
+            if(result.next()){
                 System.out.println("Quantity stock de produit " + produitID + " : ");
-                System.out.println(resultSet.getInt("quantity"));
+                System.out.println(result.getInt("quantity"));
             }else{
                 System.out.println("Aucun produit trouvé");
             }
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
+
+            Connect.closeConnexion();
+
     }
     //修改产品库存数量
-    public static void QteStockModifier(int produitID, int QteModifier){
+    public static void QteStockModifier(int produitID, int QteModifier) throws SQLException{
 
-        String sql = "UPDATE stock Set quantity = quantity + ? WHERE Id_produit = ?";
-        try (Connection connection = DriverManager.getConnection(url, user, mdp);
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, QteModifier);
-            statement.setInt(2, produitID);
+        String sql = "UPDATE stock Set quantity = quantity + " + QteModifier + " WHERE Id_produit = " + produitID;
 
-            int rowsUpdated = statement.executeUpdate();
-            if(rowsUpdated > 0){
-                System.out.println("Stock change : " + QteModifier);
-            }else {
-                System.out.println("Error! Verifiez produitID.");
-            }
+        // 执行更新 update
+        Connect.executeUpdate(sql);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println("Stock change : " + QteModifier);
+
+        Connect.closeConnexion();
+
     }
 
     //显示某类别产品的库存数量
-    public static void StockParSubCat(int catID){
-        String sql = "SELECT stock.Id_produit, SUM(stock.quantity) AS total_quantity, category FROM stock JOIN produit on stock.Id_produit = produit.Id_produit WHERE category = ? GROUP BY stock.Id_produit ";
+    public static void StockParSubCat(int catID) throws SQLException {
+        String sql = "SELECT stock.Id_produit, SUM(stock.quantity) AS total_quantity, category FROM stock JOIN produit on stock.Id_produit = produit.Id_produit WHERE category = " + catID + " GROUP BY stock.Id_produit ";
 
-        try (Connection connection = DriverManager.getConnection(url, user, mdp);
-             PreparedStatement statement = connection.prepareStatement(sql)) {
+        ResultSet result = Connect.executeQuery(sql);
 
-            statement.setInt(1, catID);
-            ResultSet resultSet = statement.executeQuery();
-
-            System.out.println("produits dans la categorie " + catID + " : ");
-            int totalStock = 0;
-            while (resultSet.next()){
-                int Qte = resultSet.getInt("total_quantity");
-                totalStock += Qte;
-                System.out.println("Produit ID : " + resultSet.getInt("Id_produit") + " QteStock : " + Qte);
-            }
-            // 显示加总数量
-            System.out.println("Total stock dans la categorie " + catID + " : " + totalStock);
-
-        }catch (Exception e) {
-            e.printStackTrace();
+        System.out.println("produits dans la categorie " + catID + " : ");
+        int totalStock = 0;
+        while (result.next()){
+            int Qte = result.getInt("total_quantite");
+            totalStock += Qte;
+            System.out.println("Produit ID : " + result.getInt("Id_produit") + " QteStock : " + Qte);
         }
+        // 显示加总数量
+        System.out.println("Total stock dans la categorie " + catID + " : " + totalStock);
 
-        }
+        Connect.closeConnexion();
+    }
+
     //修改某类别产品的库存数量
-    public static void ModifierStockParSubCat(int catID, int StockChange){
-
+    public static void ModifierStockParSubCat(int catID, int StockChange) throws SQLException{
 
         String sql = "UPDATE stock JOIN produit on stock.Id_produit = produit.Id_produit Set quantity = quantity + ? WHERE category = ?";
-        try (Connection connection = DriverManager.getConnection(url, user, mdp);
-             PreparedStatement statement = connection.prepareStatement(sql)) {
+        Connect.executeUpdate(sql);
 
-            statement.setInt(1, StockChange);
-            statement.setInt(2, catID);
+        System.out.println("Stock change : " + StockChange);
 
-            int rowsUpdated = statement.executeUpdate();
-            if(rowsUpdated > 0){
-                System.out.println("Stock change : " + StockChange);
-            }else {
-                System.out.println("Error! Verifiez ID categorie.");
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Connect.closeConnexion();
     }
 
 //将下单次数超过五次的客户设置为VIP客户
 //Définir les clients qui commandent plus de cinq fois comme des clients VIP
-    public static void VIPCLients(){
+    public static void VIPCLients() throws SQLException{
         String sql = "SELECT Id_user FROM panier JOIN PanierCommande ON panier.Id_panier = PanierCommande.panier_id GROUP by panier.Id_user HAVING COUNT(Id_commande) > 5";
 
-        try (Connection connection = DriverManager.getConnection(url, user, mdp);
-             PreparedStatement statement = connection.prepareStatement(sql)) {
+        ResultSet result = Connect.executeQuery(sql);
 
-            ResultSet resultSet = statement.executeQuery();
-            System.out.println("Les clients suivants sont des VIP :");
+        System.out.println("Les clients suivants sont des VIP :");
 
-            while (resultSet.next()){
-                int userID = resultSet.getInt("Id_user");
-                System.out.println("userID");
+        while (result.next()){
+            int userID = result.getInt("Id_user");
+            System.out.println("User ID" + userID);
 
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
+
+        Connect.closeConnexion();
     }
 
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws SQLException {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("\n=== Menu ===");
+            System.out.println("=== Menu ===");
             System.out.println("1. Rechercher un produit par mot-clé");
             System.out.println("2. Reprendre un panier en cours");
             System.out.println("3. Afficher le stock d'un produit");
@@ -197,7 +152,7 @@ public class USyihan {
                     rechercherProduitParMotCle(motCle);
                 }
                 case 2 -> {
-                    System.out.print("Entrez l'ID utilisateur : ");
+                    System.out.print("Entrez l'ID user : ");
                     int userID = scanner.nextInt();
                     PanierEnCours(userID);
                 }
